c8c384db050b735c0d5f8000e10050557b7e81af
Nerdork-40 <lovecf4444@gmail.com>
2023-11-16 22:02:58 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from query import Query
+from datetime import date, timedelta
+
+# 设置应用程序的布局为wide mode
+st.set_page_config(layout="wide")
+
+# 实例化Query对象
+query = Query()
+
+today = date.today()
+
+# 计算一个月前和一个月后的日期
+one_month_ago = today - timedelta(days=30)
+one_month_later = today + timedelta(days=30)
+
+
+# 第一个查询表单
+st.sidebar.subheader('突变日期查询')
+date1 = st.sidebar.date_input('请选择日期', min_value=one_month_ago, max_value=one_month_later, value=today)
+query_button1 = st.sidebar.button('查询', key='query1')
+
+# 第二个查询表单
+st.sidebar.subheader('突变出现日期查询')
+map_location = st.sidebar.selectbox('选择地图', query.map_location)
+game_mode = st.sidebar.selectbox('选择玩法', query.game_mode)
+date2 = st.sidebar.date_input('选择日期', min_value=one_month_ago, max_value=one_month_later, value=today)
+query_button2 = st.sidebar.button('查询', key='query2')
+
+# 主页面内容
+st.title('After the Fall 突变查询器')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    # 根据表单1的查询按钮点击状态执行查询
+    if query_button1:
+        result1 = query.query1(str(date1))
+        st.write('突变日期查询表: ')
+        st.write(result1)
+
+    # 根据表单2的查询按钮点击状态执行查询
+    if query_button2:
+        result2 = query.query2(map_location, game_mode, str(date2))
+        st.write('突变出现日期查询表: ')
+        st.write(result2)
+
+with col2:
+    st.write('After the Fall查询器使用说明：')
+    st.write('1、“突变日期查询表”， 选择日期，再点击查询，可以查询当前到指定日期期间的突变')
+    st.write('2、“突变出现日期查询表”，选择地图、玩法和日期，再点击查询，可以查询具体某个突变的出现日期。')
+    st.write('数据提供: E-11')
---
+++ query.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+class Query:
+
+    def __init__(self):
+
+        self.current_date = datetime.today().strftime('%Y-%m-%d')
+        self.df = self.generate_new_data(pd.read_excel('History_data.xlsx'))
+        self.map_location = ["市中心", "唐人街", "接力塔", "贫民窟", "联合大厦", "林荫大道", "地铁", "医院", "隔离中心"]
+        self.game_mode = ["鳄龟", "雪犁", "枪手", "15Min", "人群"]
+
+    def generate_new_data(self, df):
+        start_date = df.iloc[-1, 0]
+        one_month_later = datetime.today() + timedelta(days=30)
+        if start_date < one_month_later:
+            new_df = {'时间': [], '组合': []}
+
+            # 生成100个时间点，每个时间点间隔8小时
+            for i in range(100):
+                start_date += timedelta(hours=8)
+                new_df['时间'].append(start_date)
+                new_df['组合'].append(df.iloc[i, 1])
+
+            new_df = pd.concat([df, pd.DataFrame(new_df)], ignore_index=True)
+            new_df.to_excel('History_data.xlsx', index=False)
+            return new_df
+        else:
+            return df
+
+    def base_data(self, df):
+        # 将Date设置为index
+        df.set_index('Date', inplace=True)
+
+        df_dic = df.to_dict(orient='index')
+
+        data = []
+        count = 0
+
+        for day, row in df_dic.items():
+            for time, value in row.items():
+                if value == '联合大厦鳄龟':
+                    count += 1
+                    if count == 4:
+                        break  # 结束内部循环
+                if count != 0:
+                    data.append({'时间': datetime.combine(day, time), '组合': value})
+
+            if count == 4:
+                break  # 结束外部循环
+        result = pd.DataFrame(data)
+        return result
+    
+    def query1(self, date1):
+        if date1 > self.current_date:
+            # 返回从今天到date1的日期列表
+            date_list = pd.date_range(start=self.current_date, end=date1, freq='D').strftime('%Y-%m-%d').tolist()
+        elif date1 < self.current_date:
+            # 返回从date1到今天的日期列表
+            date_list = pd.date_range(start=date1, end=self.current_date, freq='D').strftime('%Y-%m-%d').tolist()
+        else:
+            date_list = [date1]
+        # 筛选出date_list中的数据
+        result1 = self.df[self.df['时间'].dt.strftime('%Y-%m-%d').isin(date_list)].reset_index(drop=True)
+        return result1
+    
+    def query2(self, map_location, game_mode, date2):
+        text = map_location + game_mode
+        if text not in self.df['组合'].tolist():
+            result2 = '没有该玩法'
+        else:
+            if date2 > self.current_date:
+                # 返回从今天到date1的日期列表
+                date_list = pd.date_range(start=self.current_date, end=date2, freq='D').strftime('%Y-%m-%d').tolist()
+            elif date2 < self.current_date:
+                # 返回从date1到今天的日期列表
+                date_list = pd.date_range(start=date2, end=self.current_date, freq='D').strftime('%Y-%m-%d').tolist()
+            else:
+                date_list = [date2]
+            # 根据text和date_list筛选出数据
+            result2 = self.df[(self.df['组合'] == text) & (self.df['时间'].dt.strftime('%Y-%m-%d').isin(date_list))].reset_index(drop=True)
+        return result2
+
+
+if __name__ == '__main__':
+    query = Query()
+    # result1 = query.query1('2023-09-01')
+    # result2 = query.query2('联合大厦', '鳄鱼', '2023-09-01')
+    print(query.base_data(pd.read_excel('ATF突变体.xlsx')))
